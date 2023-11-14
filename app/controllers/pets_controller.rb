501c7306975f326dbf56a4c8238467c94a55028a
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-11-14 15:27:50 -0600

    added user_id column to pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,7 @@
 class PetsController < ApplicationController
   def create
     @pet = Pet.create(
+      user_id: current_user.id,
       name: params[:name],
       age: params[:age],
       breed: params[:breed],
