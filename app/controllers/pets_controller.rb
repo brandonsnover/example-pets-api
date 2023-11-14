76132bb8fbe8912b66b2836738f27abb43c1e3fa
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-11-14 15:45:11 -0600

    added pets update action requiring login

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,11 +1,29 @@
 class PetsController < ApplicationController
   def create
-    @pet = Pet.create(
-      user_id: current_user.id,
-      name: params[:name],
-      age: params[:age],
-      breed: params[:breed],
-    )
-    render :show
+    if current_user
+      @pet = Pet.create(
+        user_id: current_user.id,
+        name: params[:name],
+        age: params[:age],
+        breed: params[:breed],
+      )
+      render :show
+    else
+      render json: { message: "must be logged in to create new pets" }
+    end
+  end
+
+  def update
+    if current_user
+      @pet = Pet.find_by(id: params[:id])
+      @pet.update(
+        name: params[:name] || @pet.name,
+        age: params[:age] || @pet.age,
+        breed: params[:breed] || @pet.breed,
+      )
+      render :show
+    else
+      render json: { message: "must be logged in to update pets" }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   post "/pets" => "pets#create"
+  patch "/pets/:id" => "pets#update"
 end
